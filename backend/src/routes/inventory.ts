58bb305aa1cfb64517e454ad292c8fb69e119dd2
Jorge Mena <jormencar@yahoo.com>
2025-03-07 08:54:51 +0000

    Refactor inventory routes to pass context to controller methods

--- backend/src/routes/inventory.ts
+++ backend/src/routes/inventory.ts
@@ -5,13 +5,13 @@ const inventoryController = new InventoryController()
 
 const inventoryRoutes = new Elysia().group('/v1/inventory', app =>
 	app
-		.get('/tables', inventoryController.getTables)
+		.get('/tables', () => inventoryController.getTables())
 		.group('/data', app =>
 			app
-				.get('/:table_name', inventoryController.getTableData)
-				.post('/:table_name', inventoryController.createTableRow)
-				.put('/:table_name', inventoryController.updateTableRow)
-				.delete('/:table_name', inventoryController.deleteTableRow)
+				.get('/:table_name', context => inventoryController.getTableData(context))
+				.post('/:table_name', context => inventoryController.createTableRow(context))
+				.put('/:table_name', context => inventoryController.updateTableRow(context))
+				.delete('/:table_name', context => inventoryController.deleteTableRow(context))
 		)
 )
 

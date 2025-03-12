6bd8e9514c7bd2bbe27a9eb68fb168250946d89e
Jorge Mena <jormencar@yahoo.com>
2025-03-12 09:42:53 +0000

    update path

--- frontend/src/lib/api-client.ts
+++ frontend/src/lib/api-client.ts
@@ -20,7 +20,7 @@ function authRequestInterceptor(config: InternalAxiosRequestConfig) {
 }
 
 export const api = axios.create({
-	baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api',
+	baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
 	headers: {
 		'Content-Type': 'application/json'
 	}

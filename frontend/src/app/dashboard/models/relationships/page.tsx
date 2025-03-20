1f012855ba9fe68196bb8eea75dcd8d269b9693c
Jorge Mena <jormencar@yahoo.com>
2025-03-20 15:02:08 +0000

    two graphs

--- frontend/src/app/dashboard/models/relationships/page.tsx
+++ frontend/src/app/dashboard/models/relationships/page.tsx
@@ -1,6 +1,7 @@
 import OrgChart from '@/features/model-relationships/components'
 import Graphs2DAnd3D from '@/components/dependency-graph/model-adapter'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import DefaultDependencyGraph from '@/components/dependency-graph/default-depdendency-graph'
 
 export default function DependencyGraphPage() {
 	return (
@@ -15,7 +16,8 @@ export default function DependencyGraphPage() {
 				</TabsContent>
 
 				<TabsContent value='model-graph'>
-					<Graphs2DAnd3D />
+					{/* <Graphs2DAnd3D /> */}
+					<DefaultDependencyGraph />
 				</TabsContent>
 			</Tabs>
 		</div>
